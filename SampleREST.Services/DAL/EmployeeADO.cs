dc14e85588a1cdbfc1398121452fd4836fea64ff
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-02-18 09:58:43 +0700

    menambahkan get by name

--- SampleREST.Services/DAL/EmployeeADO.cs
+++ SampleREST.Services/DAL/EmployeeADO.cs
@@ -54,12 +54,58 @@ namespace SampleREST.Services.DAL
 
         public IEnumerable<Employee> GetByName(string name, string city)
         {
-            throw new NotImplementedException();
+            List<Employee> employees = new List<Employee>();
+            using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+            {
+                string strSql = @"SELECT EmployeeId, EmployeeName, City FROM Employees
+                                  where EmployeeName like @EmployeeName and City like @City
+                                  order by EmployeeName asc";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                cmd.Parameters.AddWithValue("@EmployeeName", "%" + name + "%");
+                cmd.Parameters.AddWithValue("@City", "%" + city + "%");
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Employee employee = new Employee();
+                        employee.EmployeeId = Convert.ToInt32(reader["EmployeeId"]);
+                        employee.EmployeeName = reader["EmployeeName"].ToString();
+                        employee.City = reader["City"].ToString();
+                        employees.Add(employee);
+                    }
+                }
+                reader.Close();
+                cmd.Dispose();
+                conn.Close();
+            }
+            return employees;
         }
 
         public Employee GetEmployee(string id)
         {
-            throw new NotImplementedException();
+            Employee employee = new Employee();
+            using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+            {
+                string strSql = @"SELECT EmployeeId, EmployeeName, City FROM Employees
+                                  where EmployeeId = @EmployeeId";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                cmd.Parameters.AddWithValue("@EmployeeId", id);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    employee.EmployeeId = Convert.ToInt32(reader["EmployeeId"]);
+                    employee.EmployeeName = reader["EmployeeName"].ToString();
+                    employee.City = reader["City"].ToString();
+                }
+                reader.Close();
+                cmd.Dispose();
+                conn.Close();
+            }
+            return employee;
         }
 
         public Employee Insert(Employee employee)
