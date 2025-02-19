5e2ea2d29be58c32f2b28211edbe67dc260a8231
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-02-19 13:40:04 +0700

    menambahkan codefirst

---
+++ CodeFirstSample/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstSample.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; } = null!;
+
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+
+    }
+}
---
+++ CodeFirstSample/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstSample.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
+        public int Stock { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category? Category { get; set; }
+    }
+}
